86f80f71d0affe31981ee90db001b15467a7866e
smrha <lord.smrha@gmail.com>
2021-03-12 12:16:45 +0330

    add default value to the birthday

--- account/models.py
+++ account/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import now
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -7,7 +8,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     mobile = models.CharField(max_length=11, blank=True)
     address = models.CharField(max_length=255, blank=True)
-    birthday = models.DateField(blank=True)
+    birthday = models.DateField(default=now, blank=True)
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
